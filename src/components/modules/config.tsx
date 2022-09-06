03f54ab114f38b995284c480a00ac6292affa1d4
Rafael Pignataro <rafapignataro@gmail.com>
2022-09-06 17:20:28 -0300

    feat: Config password page & component

---
+++ src/components/modules/config.tsx
@@ -0,0 +1,44 @@
+import { Tabs, TabList, Tab, TabPanels, TabPanel, HStack, Flex, Avatar, VStack, FormControl, FormLabel, Input, Button } from "@chakra-ui/react"
+import { useSession } from "next-auth/react";
+
+import { AppLayout } from "../layouts/AppLayout"
+
+export const Config = () => {
+  const { data: session } = useSession();
+
+  return (
+    <AppLayout title="Configurações">
+      <Tabs flex="1" variant="soft-rounded" colorScheme="purple">
+        <TabList>
+          <HStack spacing="4">
+            <Tab>Perfil</Tab>
+            <Tab>Geral</Tab>
+          </HStack>
+        </TabList>
+        <TabPanels mt="4"  h="90%">
+          <TabPanel>
+            <Flex direction="column">
+              <Avatar src={String(session?.user?.image)} size="2xl" mb="4"/>
+              <VStack>
+                <FormControl>
+                  <FormLabel htmlFor="name">Nome</FormLabel>
+                  <Input id="name" name="name" placeholder="Nome" value={String(session?.user?.email)}/>
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="name">Nome</FormLabel>
+                  <Input id="name" name="name" placeholder="Nome" value={String(session?.user?.email)}/>
+                </FormControl>
+              </VStack>
+              <Flex align="center" justify="flex-end" mt="6" w="100%">
+                <Button colorScheme="purple" w={{ base: '100%', md: '48' }}>Salvar</Button>
+              </Flex>
+            </Flex>
+          </TabPanel>
+          <TabPanel>
+            <p>Geral</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </AppLayout>
+  )
+}
---
+++ src/pages/config.tsx
@@ -0,0 +1,28 @@
+import type { GetServerSideProps, NextPage } from 'next'
+
+import { Config } from '../components/modules/config'
+import { getServerSession } from '../utils/helpers/getServerSession'
+
+const ConfigPage: NextPage = () => <Config />
+
+export default ConfigPage
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getServerSession(ctx);
+
+  if(!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+      props: {}
+    }
+  }
+
+  return {
+    props: {
+      session,
+    }
+  }
+}
