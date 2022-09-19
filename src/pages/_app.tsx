6fffd887f1d79476966fbd36bf71ce41490d798b
Rafael Pignataro <rafapignataro@gmail.com>
2022-09-19 12:08:43 -0300

    fix: check if its production in trpc wrapper creation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,8 +19,8 @@ function App({ Component, pageProps }: AppProps) {
 
 export default withTRPC<ServerRouter>({
   config({ ctx }) {
-    const url = process.env.NEXT_PUBLIC_VERCEL_URL
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+    const url = process.env.NODE_ENV === 'production'
+      ? `https://dashix.vercel.app/api/trpc`
       : "http://localhost:3000/api/trpc";
 
     return { url };
